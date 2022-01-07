50a5603a385fd81088bd1f2d1e99635cbbc28057
Neil MacLaren <nmaclar1@binghamton.edu>
2022-01-06 21:38:37 -0500

    Improved 100 nodes sim

--- 100-nodes-forcing.R
+++ 100-nodes-forcing.R
@@ -6,6 +6,30 @@
 library(igraph)
 library(doublewells)
 
+select_stressnode <- function(g, add_stress_to = NULL) {
+    "Choose a random node to which to apply stress. Can constrain choice to either 'high' or 'low' degree nodes, in which case nodes are chosen from the top or bottom quintiles of the degree distribution, respectively. Alternatively, a random node with the 'highest' or 'lowest' (non-zero) degree can be chosen."
+    if(is.null(add_stress_to)) {
+        nnodes <- vcount(g)
+        selectnode <- sample(1:nnodes, 1)
+        return(selectnode)
+    } else {
+        k <- degree(g)
+        breaks <- quantile(k, probs = c(.2, .8))
+        if(add_stress_to == "high") {
+            poss <- V(g)[which(k >= breaks[2])]
+        } else if(add_stress_to == "low") {
+            poss <- V(g)[which(k <= breaks[1] & k > 0)]
+        } else if(add_stress_to == "highest") {
+            poss <- V(g)[which(k == max(k))]
+            if(length(poss) == 1) return(poss)
+        } else if(add_stress_to == "lowest") {
+            poss <- V(g)[which(k == min(k[which(k > 0)]))]
+        }
+        selectnode <- sample(poss, 1)
+        return(selectnode)
+    }
+}
+
 graph_choices <- c(# comments are approximate critical values of D with u = 0
     "regular", # 0.298
     "max-entropy", # 0.195
@@ -14,29 +38,39 @@ graph_choices <- c(# comments are approximate critical values of D with u = 0
     "pref-attach", # 0.067
     "small-world" # 0.24
 )
+
 graph_choice <- graph_choices[6]
-
 calc_earlywarnings <- FALSE # TRUE
-
-noise <- function(n, s) rnorm(n, 0, s)
+add_stress_to <- "lowest" # one of "highest", "high", "low", "lowest", or NULL
+linear_increase <- FALSE # TRUE, FALSE, or NULL (for no increase)
 
 nnodes <- 100
 r1 <- 1 # lower equil
 r2 <- 3 # separatrix
 r3 <- 5 # upper equil
 s <- .005 # noise parameter
-D <- NULL # connection strength
-##u <- 3  # stress parameter, add it to one node
+D <- NULL # connection strength; can set here or let the code below set the value to just below the approximate critical threshold for each graph type.
+maxU <- 2
 dt <- 0.01
 T <- 5000
-usteps <- T
-U <- seq(0, 0, length.out = usteps)
+if(is.null(linear_increase)) { # for completeness, when forcing on u is not desired
+    U <- seq(0, 0, length.out = T)
+} else if(linear_increase) {
+    usteps <- T
+    U <- seq(0, maxU, length.out = usteps)
+} else {
+    usteps <- 2000
+    U <- c(seq(0, maxU, length.out = usteps), rep(maxU, T - usteps))
+}
+   
+noise <- function(n, s) rnorm(n, 0, s) # the white noise function
+
 
 ## target density approx .06
 ## this needs to be balanced with D and u, as well as the small world and random regular networks, which are limited in possible values for density
 cprob <- .06
 if(graph_choice == "max-entropy") {
-    if(is.null(D)) D <- 0.19
+    if(is.null(D)) D <- 0.18
     g <- sample_gnp(nnodes, cprob)
     main <- "Maximum Entropy"
 } else if(graph_choice == "sphere-surface") {
@@ -44,7 +78,7 @@ if(graph_choice == "max-entropy") {
     g <- sample_dot_product(sample_sphere_surface(dim = 3, n = nnodes, radius = .279)) # .35 is ~ .1
     main <- "Sphere Surface/Dot-Product"
 } else if(graph_choice == "islands") {
-    if(is.null(D)) D <- 0.19
+    if(is.null(D)) D <- 0.18
     nislands <- 5
     nbridges <- 1
     base_prob <- cprob*nislands
@@ -78,7 +112,7 @@ initialx <- rep(1, nnodes)
 results <- matrix(0, nrow = T, ncol = nnodes)
 
 stress <- rep(0, nnodes)
-stressnode <- sample(1:nnodes, 1)
+stressnode <- select_stressnode(g, add_stress_to = add_stress_to) # "lowdegree")
 
 x <- initialx
 
@@ -96,13 +130,11 @@ V(g)$nodestate <- x/max(x)
 colorfun <- colorRamp(c("blue", "orange"), space = "Lab")
 nodecolor <- colorfun(V(g)$nodestate)
 V(g)$color <- apply(nodecolor, 1, function(x) rgb(t(x), maxColorValue = 255))
-##nodecolor <- ifelse(stress == 0, "orange", "dodgerblue")
 nodesize <- ifelse(stress == 0, 4, 8)
 linewidth <- ifelse(stress == 0, .2, 2)
 plot(
     g, vertex.label = "",  main = main,
     vertex.size = nodesize
-    ##vertex.color = nodecolor
 )
 matplot(
     1:T, results, type = "l",
@@ -112,8 +144,8 @@ matplot(
     xlab = "t", ylab = expression(x[i]), main = "Node States"
 )
 
+## Early Warning
 if(calc_earlywarnings) {
-    ## Early Warning
     acresults <- apply(results, 2, function(x) windowed_acmethod(x, wl = T/4))
 
     dev.new()
@@ -126,3 +158,5 @@ if(calc_earlywarnings) {
         main = "Node-Level Early Warning Signals"
     )
 }
+
+print(paste0("Degree of stress node is ", degree(g, V(g)[stressnode])))
